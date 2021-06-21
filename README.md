fc9d3459fc0265bab9b4635533ee521b8b49f82c
Marius Sarca <marius.sarca@gmail.com>
2021-06-22 00:08:25 +0300

    Refactored build process (#36)

---
+++ README.md
@@ -0,0 +1,18 @@
+<p align="center"><img width="150" src="https://raw.githubusercontent.com/ExpressiveSuite/Art/master/logo/expressive-animator.svg"><h1 align="center">Expressive Animator</h1></p>
+
+Create stunning 2D motion graphics and vector animations for any platform in a matter of minutes, 
+whether you’re a beginner or a professional.
+
+## Installation
+
+The package can be installed by issuing the following command
+
+```shell
+npm install @zindex/expressive-animator
+```
+
+Or, if you are a Yarn user
+
+```shell
+yarn add @zindex/expressive-animator
+```
--- rollup.config.js
+++ rollup.config.js
@@ -17,35 +17,14 @@ const GLOBALS = {
 };
 const EXTERNAL = Object.keys(GLOBALS);
 
-function serve() {
-  let server;
-
-  function toExit() {
-    if (server) server.kill(0);
-  }
-
-  return {
-    writeBundle() {
-      if (server) return;
-      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-        stdio: ['ignore', 'inherit', 'inherit'],
-        shell: true
-      });
-
-      process.on('SIGTERM', toExit);
-      process.on('exit', toExit);
-    }
-  };
-}
-
-export default [
+const CONFIG = [
   {
     input: 'src/index.ts',
     output: {
       sourcemap: true,
       format: 'iife',
       name: 'app',
-      file: 'public/build/bundle.js',
+      file: 'dist/expressive-animator.js',
       globals: GLOBALS,
     },
     external: EXTERNAL,
@@ -56,11 +35,11 @@ export default [
       }),
       svelte({
         preprocess: sveltePreprocess({
-          sourceMap: !production
+          sourceMap: true
         }),
         compilerOptions: {
           // enable run-time checks when not in production
-          dev: !production
+          dev: true
         }
       }),
       svg(),
@@ -68,11 +47,9 @@ export default [
       // a separate file - better for performance
       // css({output: 'bundle.css'}),
       postcss({
-        extract: 'bundle.css',
+        extract: 'expressive-animator.css',
         plugins: [
           postcssImport,
-          // postcssVarFallback,
-          production && cssnano,
         ],
       }),
       // If you have external dependencies installed from
@@ -89,20 +66,70 @@ export default [
         sourceMap: false,
         inlineSources: false
       }),
-      // In dev mode, call `npm run start` once
-      // the bundle has been generated
-      !production && serve(),
-
-      // Watch the `public` directory and refresh the
-      // browser on changes when not in production
-      !production && livereload('public'),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      production && terser()
     ],
     watch: {
       clearScreen: false
     }
   }
 ];
+
+if (production) {
+  CONFIG.push({
+    input: 'src/index.ts',
+    output: {
+      sourcemap: false,
+      format: 'iife',
+      name: 'app',
+      file: 'dist/expressive-animator.min.js',
+      globals: GLOBALS,
+    },
+    external: EXTERNAL,
+    plugins: [
+      importResolver({
+        extensions: ['.js', '.mjs', '.ts', '.css', '.pcss', '.svelte'],
+        packageJson: true,
+      }),
+      svelte({
+        preprocess: sveltePreprocess({
+          sourceMap: false
+        }),
+        compilerOptions: {
+          // enable run-time checks when not in production
+          dev: false
+        }
+      }),
+      svg(),
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      // css({output: 'bundle.css'}),
+      postcss({
+        extract: 'expressive-animator.min.css',
+        plugins: [
+          postcssImport,
+          // postcssVarFallback,
+          cssnano,
+        ],
+      }),
+      // If you have external dependencies installed from
+      // npm, you'll most likely need these plugins. In
+      // some cases you'll need additional configuration -
+      // consult the documentation for details:
+      // https://github.com/rollup/plugins/tree/master/packages/commonjs
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
+      }),
+      commonjs(),
+      typescript({
+        sourceMap: false,
+        inlineSources: false
+      }),
+      terser()
+    ],
+    watch: {
+      clearScreen: false
+    }
+  });
+}
+
+export default CONFIG;
